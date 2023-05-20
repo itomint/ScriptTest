8f35326a17f9aa6080ef43f87f2832216e061397
itomint <shun6110oti@gmail.com>
2023-05-20 19:03:31 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    private int hp = 100;          // 体力
+    private int power = 25; // 攻撃力
+    private int mp = 53;
+
+    // 攻撃用の関数
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    // 防御用の関数
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        // 残りhpを減らす
+        this.hp -= damage;
+    }
+
+    public void Magic(int mahou)
+    {
+        Debug.Log(mahou + "のダメージを与えた");
+
+      if(this.mp >= 5)
+        {
+           int c = this.mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + c);
+        }
+      else
+        {
+            Debug.Log("MPが足りない、魔法が使用できません。");
+        }
+    }
+}
+
+public class Test : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+       
+        Boss lastboss = new Boss();
+
+       
+        lastboss.Attack();
+        
+        lastboss.Defence(3);
+
+        for(int i = 0; i<=10; i++)
+        {
+            lastboss.Magic(150);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
